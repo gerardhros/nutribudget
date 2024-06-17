122455db1a2080b4946c2d179da2dc4519badeab
Hongzhen <142984074+hongzhenhere@users.noreply.github.com>
2024-06-17 11:45:15 +0200

    Update q3 analysis.R

--- scripts/q3 analysis.R
+++ scripts/q3 analysis.R
@@ -34,7 +34,7 @@ table(d1$kpi_type)
 # --- Analysis for KPI 1 -----
   
   # subset the file
-  d2 <- d1[kpi_type =='DGR']
+  d2 <- d1[kpi_type =='ADG']
 
   # remove observations without a control or treatment value
   d2 <- d2[!(is.na(kpi_treat_mean) | is.na(kpi_control_mean))]
@@ -67,8 +67,8 @@ table(d1$kpi_type)
     geom_point() + 
     geom_line()+
     geom_errorbar(aes(x=id,ymin = yi-vi,ymax=yi+vi))+
-    theme_bw() + xlab('study id') + ylab('lnRR, change in DGR') + 
-    ggtitle('Observed changes in DGR due to novel protein source')
+    theme_bw() + xlab('study id') + ylab('lnRR, change in average daily weight gain (ADG)') + 
+    ggtitle('Observed changes in average daily weight gain (ADG) due to alternative protein source')
 
   # estimate mean response across all studies
   m1 <- metafor::rma.mv(yi,vi, 
@@ -137,7 +137,7 @@ table(d1$kpi_type)
   ggplot(data=out.dgr[var!='intercept']) +
     geom_bar(aes(x=estimate,y= reorder(var,pfactor),group = pfactor,fill=pfactor),stat="identity") + 
     geom_errorbar(aes(y=var,xmin = estimate - se,xmax = estimate +se),width=0.4) + theme_bw() +
-    ggtitle('effect of main factors on DGR') +
+    ggtitle('effect of main factors on average daily weight gain (ADG)') +
     theme(legend.position = 'bottom')
   
   # do one meta-regression model with multiple factors
@@ -213,9 +213,9 @@ table(d1$kpi_type)
               vjust = ifelse(m3_tidy$estimate > 0, -0.5, 1.5), size = 5) +
     theme_minimal() +
     scale_x_discrete(labels = abbreviations)+ 
-    labs(x = 'animal management practice & factors',
+    labs(x = 'animal management practice & influencing factors',
          y = 'Parameter estimate',
-         title = 'Meta-regression model estimation on the daily growth rate (DGR)') +
+         title = 'Meta-regression model estimation on the average daily weight gain (ADG)') +
     theme(axis.text.x = element_text(angle = 90, hjust = 1),
           panel.border = element_rect(color = "black", fill = NA, size = 1),  # Add black frame line
           axis.ticks.length = unit(-0.5, "cm"),  # Minor tick marks outside
@@ -254,8 +254,8 @@ table(d1$kpi_type)
     geom_point() + 
     geom_line()+
     geom_errorbar(aes(x=id,ymin = yi-vi,ymax=yi+vi))+
-    theme_bw() + xlab('study id') + ylab('lnRR, change in DCP') + 
-    ggtitle('Observed changes in DCP due to novel protein source')
+    theme_bw() + xlab('study id') + ylab('lnRR, change in the digestibility of crude protein (DCP)') + 
+    ggtitle('Observed changes in the digestibility of crude protein (DCP) due to alternative protein source')
   
   # estimate mean response across all studies
   m1 <- metafor::rma.mv(yi,vi, 
@@ -325,7 +325,7 @@ table(d1$kpi_type)
   ggplot(data=out.dcp[var!='intercept']) +
     geom_bar(aes(x=estimate,y= reorder(var,pfactor),group = pfactor,fill=pfactor),stat="identity") + 
     geom_errorbar(aes(y=var,xmin = estimate - se,xmax = estimate +se),width=0.4) + theme_bw() +
-    ggtitle('effect of main factors on DCP') +
+    ggtitle('effect of main factors on digestability of crude protein (DCP)') +
     theme(legend.position = 'bottom')
   
   # do one meta-regression model with multiple factors
@@ -377,7 +377,7 @@ table(d1$kpi_type)
                                         ifelse(m3_tidy$p.value < 0.05, "*", 
                                                ifelse(m3_tidy$p.value < 0.1, ".",""))))
   # Named vector with abbreviations
-  abbreviations <- c("supplemental_rate"="rate","supplement_categorylegumes"="legumes","supplement_categorymicroalgae"="microalgae","supplement_categoryrapeseed"="rapeseed","stagegrowing"="growing","stagewhole"="whole","supplemental_rate:supplement_categorylegumes"="rate+legumes","supplemental_rate:supplement_categoryrapeseed"="rate+rapeseed","intercept"="intrcpt")
+  abbreviations <- c("supplemental_rate"="rate","supplement_categorylegumes"="legumes","supplement_categorymicroalgae"="microalgae","supplement_categoryrapeseed"="rapeseed","stagegrowing"="growing","stagewhole"="whole","supplemental_rate:supplement_categorylegumes"="rate+legumes","supplemental_rate:supplement_categoryrapeseed"="rate+rapeseed","intercept"="intercept")
   # Define the order of the terms
   m3_tidy$term <- factor(m3_tidy$term, levels = c("supplemental_rate","supplement_categorylegumes","supplement_categorymicroalgae","supplement_categoryrapeseed","supplemental_rate:supplement_categorylegumes","stagegrowing","stagewhole","supplemental_rate:supplement_categoryrapeseed","intercept"))
   
@@ -401,162 +401,7 @@ table(d1$kpi_type)
    
 # --- Analysis for KPI 3 -----
   ## this KPI is excluded in D1.3, as the dataset is not enough to develop a meta-regression model
-  # there are quite some cases without control. 
-  
-  # subset the file
-  d2 <- d1[kpi_type =='NUE']
-  
-  # remove observations without a control or treatment value
-  d2 <- d2[!(is.na(kpi_treat_mean) | is.na(kpi_control_mean))]
-  # check the data.table on missing values
-  summary(d2)
-  
-  # rename the column with a difficult name to select
-  setnames(d2,old = 'duration_days', new = 'duration_days')
-  
-  # i see that duration has 3 missing values, so i replaced these values by the median value
-  d2[is.na(duration_days), duration_days := median(d2$duration_days,na.rm=TRUE)]
-  
-  # add response ratio as effect size; you can select another by adapting "measure" argument
-  d2 <- escalc(measure = "ROM", data = d2, 
-               m1i = kpi_treat_mean , sd1i = kpi_treat_sd , n1i = replication,
-               m2i = kpi_control_mean, sd2i = kpi_control_sd, n2i = replication)
-  
-  # convert back to data.table
-  d2 <- as.data.table(d2)
-  
-  # remove some columns to make visualisation in console easier
-  d2[,c('location','man_treatment','man_control','control_composition','year','mat','map') := NULL]
-  
-  # add id based on order yi
-  d2[, id := frankv(yi,order = -1)]
-  
-  # make first plot of individual observations
-  ggplot(data = d2,aes(x= id,y=yi)) + 
-    geom_point() + 
-    geom_line()+
-    geom_errorbar(aes(x=id,ymin = yi-vi,ymax=yi+vi))+
-    theme_bw() + xlab('study id') + ylab('lnRR, change in NUE') + 
-    ggtitle('Observed changes in NUE due to novel protein source')
-  
-  # estimate mean response across all studies
-  m1 <- metafor::rma.mv(yi,vi, 
-                        data=d2,
-                        random= list(~ 1|study_ID), 
-                        method="REML",
-                        sparse = TRUE)
-  
-  # see the summary of the model
-  summary(m1)
-  
-  # analyse whether the mean response differs per factor
-  fcols <- c('animal_type','supplement_category','supplemental_rate','stage')
-  
-  # make an empty list to store output
-  out.nue <- list()
-  
-  # do the analysis in a for loop
-  for(i in fcols){
-    
-    # make a temporary variable 'evar'
-    d2[,evar := get(i)]
-    
-    # is the variable categorial or not
-    vartype = is.character(d2[,get(i)])
-    
-    # do the meta-analysis for categorial variable
-    if(vartype==TRUE){
-      
-      # do the meta-analysis
-      m2 <- metafor::rma.mv(yi,vi, mods = ~factor(evar)-1,data=d2,
-                            random= list(~ 1|study_ID), method="REML",sparse = TRUE)
-      
-      # collect model coefficients
-      m2.sum <- summary(m2)
-      m2.out <- as.data.table(coefficients(m2.sum))
-      m2.out[,factor := i]
-      m2.out[,var := gsub('factor\\(evar\\)','',rownames(m2.sum$b))]
-      
-    } else{
-      
-      # do the meta-analysis
-      m2 <- metafor::rma.mv(yi,vi, mods = ~evar,data=d2,
-                            random= list(~ 1|study_ID), method="REML",sparse = TRUE)
-      
-      # collect model coefficients
-      m2.sum <- summary(m2)
-      m2.out <- as.data.table(coefficients(m2.sum))
-      m2.out[,factor := i]
-      m2.out[,var := c('intercept',i)]
-      
-    }
-    
-    # add kpi type
-    m2.out[,kpi :='NUE']
-    
-    # add to list
-    out.nue[[i]] <- copy(m2.out)
-  }
-  
-  sapply(lapply(d2, unique), length)
-  # convert list to one data.table
-  out.nue <- rbindlist(out.nue)
-  
-  # make barplot (figure formatting: need to be done)
-  out.nue[, pfactor := as.factor(factor)]
-  ggplot(data=out.nue[var!='intercept']) +
-    geom_bar(aes(x=estimate,y= reorder(var,pfactor),group = pfactor,fill=pfactor),stat="identity") + 
-    geom_errorbar(aes(y=var,xmin = estimate - se,xmax = estimate +se),width=0.4) + theme_bw() +
-    ggtitle('effect of main factors on NUE') +
-    theme(legend.position = 'bottom')+
-    scale_y_discrete(limits = c("supplemental_rate","chichen","pig", "starter", "growing","finishing","whole", "rapeseed","legumes", "insect", "duckweed","microalgae"))
-  
-  # do one meta-regression model with multiple factors
-  
-  # make a function to extract relevant model statistics
-  estats <- function(model_new,model_base){
-    out <- data.table(AIC = model_new$fit.stats[4,2],
-                      ll = model_new$fit.stats[1,2],
-                      ll_impr = round(100 * (1-model_new$fit.stats[1,2]/model_base$fit.stats[1,2]),2),
-                      r2_impr = round(100*max(0,(sum(model_base$sigma2)-sum(model_new$sigma2))/sum(model_base$sigma2)),2),
-                      pval = round(anova(model_new,model_base)$pval,3))
-    return(out)
-  }
-  
-  # make first an empty model
-  m3.empty <- metafor::rma.mv(yi,vi, data=d2,random= list(~ 1|study_ID), method="REML",sparse = TRUE)
-  
-  # make a full model with all main factors together    
-  m3.full <- metafor::rma.mv(yi,vi, 
-                             mods = ~animal_type + supplement_category + supplemental_rate + stage,
-                             data=d2,random= list(~ 1|study_ID), method="REML",sparse = TRUE)
-  
-  # analyse summary stats
-  summary(m3.full)
-  
-  # refine the model (if desired)
-  # steps to do: add variables plus interactions (* for all interactions plus main factors, : for interactions only), check pvalue
-  # if within a variable one subgroup is significant and others not, then adjust the groupings
-  # so, you have the options:
-  # only a model with additive factors => use the plus sign in the mods argument
-  # if you have a model with main factors AND interactions => use the "*" sign in the mods argument
-  # if you have a model with ONLY interactions => use then the ":" sign
-  d2[,sup_cat := fifelse(grepl('insect',supplement_category),'insect','other')] 
-  d2[,early_stage := fifelse(grepl('finishing|whole',stage),'other',stage)] 
-  # Find the unique values for each variable
-  sapply(lapply(d2, unique), length)
-  m3.full <- metafor::rma.mv(yi,vi, 
-                             mods = ~ supplemental_rate + animal_type +early_stage + early_stage * sup_cat,
-                             data=d2,random= list(~ 1|study_ID), method="REML",sparse = TRUE)
-  
-  # analyse summary stats
-  summary(m3.full)
-  
-  # collect stats of the model
-  estats(m3.full,m3.empty)
-  
-  # show anova whether full model is signifiantly different from empty model
-  anova(m3.full,m3.empty,refit = T)
+
 
 
 # --- Analysis for KPI 4 -----
@@ -596,8 +441,8 @@ table(d1$kpi_type)
     geom_point() + 
     geom_line()+
     geom_errorbar(aes(x=id,ymin = yi-vi,ymax=yi+vi))+
-    theme_bw() + xlab('study id') + ylab('lnRR, change in FCR') + 
-    ggtitle('Observed changes in FCR due to novel protein source')
+    theme_bw() + xlab('study id') + ylab('lnRR, change in feed conversion ratio (FCR)') + 
+    ggtitle('Observed changes in feed conversion ratio (FCR) due to alternative protein source')
   
   # estimate mean response across all studies
   m1 <- metafor::rma.mv(yi,vi, 
@@ -666,7 +511,7 @@ table(d1$kpi_type)
   ggplot(data=out.fcr[var!='intercept']) +
     geom_bar(aes(x=estimate,y= reorder(var,pfactor),group = pfactor,fill=pfactor),stat="identity") + 
     geom_errorbar(aes(y=var,xmin = estimate - se,xmax = estimate +se),width=0.4) + theme_bw() +
-    ggtitle('effect of main factors on FCR') +
+    ggtitle('effect of main factors on feed conversion ratio (FCR)') +
     theme(legend.position = 'bottom')+
     scale_y_discrete(limits = c("supplemental_rate","pig", "starting", "growing","finishing","whole", "rapeseed","legumes", "insect", "grassjuice","duckweed","microalgae"))
   
@@ -733,7 +578,7 @@ table(d1$kpi_type)
                                                ifelse(m3_tidy$p.value < 0.1, ".",""))))
   
   # Named vector with abbreviations
-  abbreviations <- c("supplemental_rate"="rate","supplement_categorygrassjuice"="grassjuice","supplement_categoryinsect"="insect","supplement_categorylegumes"="legumes","supplement_categorymicroalgae"="microalage","supplement_categoryrapeseed"="rapeseed","supplemental_rate:supplement_categorygrassjuice"="rate+grassjuice","supplemental_rate:supplement_categoryinsect"="rate+insect","supplemental_rate:supplement_categorylegumes"="rate+legumes","supplemental_rate:supplement_categorymicroalgae"="rate+microalgae","supplemental_rate:supplement_categoryrapeseed"="rate+rapeseed","supplement_categoryinsect:stagegrowing"="insect at growing stage","supplement_categorylegumes:stagegrowing"="legumes at growing stage","animal_typepig"="pig","stagegrowing"="growing","stagestarting"="starting","stagewhole"="whole","intercept"="intrcpt")
+  abbreviations <- c("supplemental_rate"="rate","supplement_categorygrassjuice"="grassjuice","supplement_categoryinsect"="insect","supplement_categorylegumes"="legumes","supplement_categorymicroalgae"="microalage","supplement_categoryrapeseed"="rapeseed","supplemental_rate:supplement_categorygrassjuice"="rate+grassjuice","supplemental_rate:supplement_categoryinsect"="rate+insect","supplemental_rate:supplement_categorylegumes"="rate+legumes","supplemental_rate:supplement_categorymicroalgae"="rate+microalgae","supplemental_rate:supplement_categoryrapeseed"="rate+rapeseed","supplement_categoryinsect:stagegrowing"="insect at growing stage","supplement_categorylegumes:stagegrowing"="legumes at growing stage","animal_typepig"="pig","stagegrowing"="growing","stagestarting"="starting","stagewhole"="whole","intercept"="intercept")
   # Define the order of the terms
   m3_tidy$term <- factor(m3_tidy$term, levels = c("supplemental_rate","supplement_categorygrassjuice","supplement_categoryinsect","supplement_categorylegumes","supplement_categorymicroalgae","supplement_categoryrapeseed","supplemental_rate:supplement_categorygrassjuice","supplemental_rate:supplement_categoryinsect","supplemental_rate:supplement_categorylegumes","supplemental_rate:supplement_categorymicroalgae","supplemental_rate:supplement_categoryrapeseed","supplement_categoryinsect:stagegrowing","supplement_categorylegumes:stagegrowing","animal_typepig","stagegrowing","stagestarting","stagewhole","intercept"))
   
@@ -744,7 +589,7 @@ table(d1$kpi_type)
               vjust = ifelse(m3_tidy$estimate > 0, -0.5, 1.5), size = 3) +
     theme_minimal() +
     scale_x_discrete(labels = abbreviations)+ 
-    labs(x = 'animal management practice & factors',
+    labs(x = 'animal management practice & influencing factors',
          y = 'Parameter estimate',
          title = 'Meta-regression model estimation on the feed conversion ratio (FCR)') +
     theme(axis.text.x = element_text(angle = 90, hjust = 1),
